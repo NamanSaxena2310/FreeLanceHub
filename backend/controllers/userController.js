28f6553771e3bbb23f170ecb4f65ecfc6561487e
NamanSaxena2310git config --global user.name NamanSaxena2310git config --global user.name NamanSaxena2310 <namansaxena69@gmail.com>
2025-05-23 16:47:29 -0400

    Login Api implemenmted

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -62,7 +62,47 @@ const registerUser = async(req,res,next)=>{
 // Login Controller
 
 const loginUser = async(req,res,next)=>{
-  
+  try {
+    const {email,password} = req.body
+      if (!validator.isEmail(email)) {
+      return res.json({
+          success:false,
+          message: "Please enter a valid email"
+        })
+     }
+
+     const customerDetails = await User.findOne({email})
+
+
+     if (!customerDetails) {
+          return res.json({
+            success:false,
+            message:"Email does not exist in our database please register first"
+          })
+     }
+
+     const isMatch = await bcrypt.compare(password,customerDetails.password)
+
+     if (isMatch) {
+        const token = createToken(customerDetails._id)
+        return res.json({
+          success:true,
+          token:token
+        })
+     }
+
+     res.json({
+      success:false,
+      message:"Incorrect password"
+     })
+
+  } catch (error) {
+    console.log(error)
+    res.json({
+          success:false,
+          message: error.message
+        })
+  }
 }
 
 module.exports = {
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express")
-const { registerUser } = require("../controllers/userController")
+const { registerUser, loginUser } = require("../controllers/userController")
 
 
 const userRouter = new express.Router()
 
 userRouter.post('/register',registerUser)
-userRouter.post('/login',registerUser)
+userRouter.post('/login',loginUser)
 
 
 module.exports= {userRouter}
